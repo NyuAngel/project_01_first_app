05f0d66588db5cb16e686550017892e1dc8591e8
Viviana Angel <lvfandino@misena.edu.co>
2024-06-27 17:50:19 -0500

    my first component

---
+++ src/components/MyFirstComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const MyFirstComponent = () => {
+  return (
+    <>
+    <div>MyFirstComponent</div>
+    <div>Hello World, I'm Lady Viviana Fandiño Angel</div>
+    <div>27 June 2024 </div>
+    </>
+  )
+}
